78bedd9e57de1b460dafde5acd2a621a1c4b8fd2
starirene9 <>
2025-10-14 14:33:14 +0900

    maxheap question

---
+++ 4th_week/04_08_get_minimum_count_of_overseas_supply.py
@@ -0,0 +1,33 @@
+import heapq
+
+ramen_stock = 4
+supply_dates = [4, 10, 15]
+supply_supplies = [20, 5, 10]
+supply_recover_k = 30
+
+# 현재 재고가 바닥나는 시점 이전까지 받을 수 있는 밀가루 중 제일 많은 밀가루를 받는게 목표
+'''
+1. 현재 재고의 상태에 따라 최고값을 받아야 한다.
+2. 제일 많은 값만 가져가면 된다.
+-> maxHeap  
+'''
+def get_minimum_count_of_overseas_supply(stock, dates, supplies, k):
+    # 풀어보세요!
+    answer = 0
+    last_added_date_index = 0
+    max_heap = []
+    while stock <= k:
+        while last_added_date_index < len(dates) and dates[last_added_date_index] <= stock:
+            heapq.heappush(max_heap, supplies[last_added_date_index] * -1)
+            last_added_date_index += 1
+        supply =  heapq.heappop(max_heap) * -1
+        stock += supply
+        answer += 1
+
+    return answer
+
+
+print(get_minimum_count_of_overseas_supply(ramen_stock, supply_dates, supply_supplies, supply_recover_k))
+print("정답 = 2 / 현재 풀이 값 = ", get_minimum_count_of_overseas_supply(4, [4, 10, 15], [20, 5, 10], 30))
+print("정답 = 4 / 현재 풀이 값 = ", get_minimum_count_of_overseas_supply(4, [4, 10, 15, 20], [20, 5, 10, 5], 40))
+print("정답 = 1 / 현재 풀이 값 = ", get_minimum_count_of_overseas_supply(2, [1, 10], [10, 100], 11))
